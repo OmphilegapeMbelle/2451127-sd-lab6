f1d8423e7fb66bb94d5ee5314b823189c3fecbb2
Omphilegape Mbelle <2451127@students.wits.ac.za>
2024-04-09 18:47:49 +0200

    ' '

---
+++ api/cars/:id/index.js
@@ -0,0 +1,21 @@
+module.exports = async function (context, req) {
+    const cars = require('./cars.json');
+    const name = (req.query.name || (req.body && req.body.name));;
+    let responseMessage = cars;
+
+    
+    if (req.url === 'https://2992a9f0-a1b2-4df8-b9e6-19f659867cd2.azurewebsites.net/api/cars/:id'){
+        responseMessage = car;
+    }
+
+    /*if (req.url === 'https://2992a9f0-a1b2-4df8-b9e6-19f659867cd2.azurewebsites.net/api/cars'){
+        const responseMessage = cars;
+    }*/
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+
+    
+}
--- api/cars/index.js
+++ api/cars/index.js
@@ -7,9 +7,6 @@ module.exports = async function (context, req) {
     if (req.url === 'https://2992a9f0-a1b2-4df8-b9e6-19f659867cd2.azurewebsites.net/api/cars' && (req.method === 'GET' || req.method === 'PUT')){
         responseMessage = cars;
     }
-    if (req.url === 'https://2992a9f0-a1b2-4df8-b9e6-19f659867cd2.azurewebsites.net/api/cars/:id'){
-        responseMessage = car;
-    }
 
     /*if (req.url === 'https://2992a9f0-a1b2-4df8-b9e6-19f659867cd2.azurewebsites.net/api/cars'){
         const responseMessage = cars;
