34ca03523e7e287fdf122669aef71fcc9237e23f
Omphilegape Mbelle <2451127@students.wits.ac.za>
2024-04-09 11:10:21 +0200

    ' '

--- api/cars/index.js
+++ api/cars/index.js
@@ -9,6 +9,7 @@ const cars = require('./cars.json');
 
 
 module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
     context.res = {
         body: cars
     };
