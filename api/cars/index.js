51dda2c52c8adb5b71fd1617972c7292da2d8ddd
Omphilegape Mbelle <2451127@students.wits.ac.za>
2024-04-09 13:19:39 +0200

    ' '

--- api/cars/index.js
+++ api/cars/index.js
@@ -1,12 +1,19 @@
 //create cars api using express
+const axios = require('axios');
+
 module.exports = async function (context, req) {
-    const name = (req.query.name || (req.body && req.body.name));;
-    const responseMessage = req;
+    try {
+        const response = await axios.get('https://e08883b3-7e72-4f59-b2ca-fd1e00a32bae.azurewebsites.net/api/cars');
+        const cars = response.data; // Assuming the API returns an array of cars
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-
-    
-}
+        context.res = {
+            status: 200,
+            body: cars
+        };
+    } catch (error) {
+        context.res = {
+            status: error.response.status || 500,
+            body: error.response.data || 'Internal Server Error'
+        };
+    }
+};
