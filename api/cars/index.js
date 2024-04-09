f1f8ccb6345670f2892d2222410fc8c2316c00ae
Omphilegape Mbelle <2451127@students.wits.ac.za>
2024-04-09 15:01:06 +0200

    ' '

--- api/cars/index.js
+++ api/cars/index.js
@@ -2,11 +2,11 @@
 module.exports = async function (context, req) {
     const cars = require('./cars.json');
     const name = (req.query.name || (req.body && req.body.name));;
+    let responseMessage = cars;
 
-    /*if (req.url === 'https://2992a9f0-a1b2-4df8-b9e6-19f659867cd2.azurewebsites.net/api/cars' && req.method === 'GET'){
-        const responseMessage = cars;
-    }*
-
+    if (req.url === 'https://2992a9f0-a1b2-4df8-b9e6-19f659867cd2.azurewebsites.net/api/cars' && req.method === 'GET'){
+        responseMessage = cars;
+    }
     /*if (req.url === 'https://2992a9f0-a1b2-4df8-b9e6-19f659867cd2.azurewebsites.net/api/cars/:id' && req.method === 'GET'){
         const responseMessage = car;
     }*/
@@ -15,7 +15,7 @@ module.exports = async function (context, req) {
         const responseMessage = cars;
     }*/
 
-    const responseMessage = cars;
+    //responseMessage = cars;
 
     context.res = {
         // status: 200, /* Defaults to 200 */
