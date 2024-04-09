fe255785eab417f06c1c1d3ed49f90a5c2596c62
Omphilegape Mbelle <2451127@students.wits.ac.za>
2024-04-09 11:28:59 +0200

    ' '

--- api/cars/index.js
+++ api/cars/index.js
@@ -1,19 +1,53 @@
-module.exports = async function (context, req) {
-    const express = require('express');
-    const app = express();
+//create cars api using express
+const express = require('express');
+const app = express();
 
-    //console.log('Listening on 3000');
-    app.use(express.json());
+//console.log('Listening on 3000');
+app.use(express.json());
 
-    const cars = require('./cars.json');
-    const seeds = cars;
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function hgdtfgtee successfully. Pass a name in the query string or in the request body for a personalized response.";
+const cars = require('./cars.json');
 
-    context.log(cars);
-    context.res = {
-        body: responseMessage
-    };
-}
+//get all cars
+app.get('/cars', (req, res) => {
+    res.send(cars);
+});
+
+//get car by id
+app.get('/cars/:id', (req, res) => {
+    const id = req.params.id;
+    const car = cars[req.params.id];
+    res.send(car);
+});
+
+//update car
+app.put('/cars/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedCar = req.body;
+    const index = cars.findIndex(car => car.id === id);
+    cars[req.params.id] = updatedCar;
+
+    res.send(updatedCar);
+});
+
+//delete car
+app.delete('/cars/:id', (req, res) => {
+    const id = req.params.id;
+    const index = cars.findIndex(car => car.id === id);
+    cars.splice(index, 1);
+    res.json({ message: `Car with id ${id} deleted` });
+});
+
+//add car
+app.post('/cars', (req, res) => {
+
+    const newCar = req.body;
+    newCar.id = cars.length + 1;
+    console.log(newCar);
+    cars.push(newCar);
+    res.json(newCar);
+});
+
+//start app at localhost:3001
+app.listen(3000, () => {
+    console.log('Server started at https://mango-desert-0addf3403.5.azurestaticapps.net');
+});
