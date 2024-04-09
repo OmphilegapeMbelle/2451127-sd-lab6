e06c82843b6945de4bf71bf1da014a8fa8624db3
Omphilegape Mbelle <2451127@students.wits.ac.za>
2024-04-09 13:43:53 +0200

    ' '

--- api/cars/index.js
+++ api/cars/index.js
@@ -1,10 +1,4 @@
 //create cars api using express
-const express = require('express');
-const app = express();
-
-//console.log('Listening on 3000');
-app.use(express.json());
-
 module.exports = async function (context, req) {
     const cars = require('./cars.json');
     const name = (req.query.name || (req.body && req.body.name));;
