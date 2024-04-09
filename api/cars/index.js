d11a2c7123501866def723276d21f441c9bb8871
Omphilegape Mbelle <2451127@students.wits.ac.za>
2024-04-09 11:12:45 +0200

    ' '

--- api/cars/index.js
+++ api/cars/index.js
@@ -9,9 +9,18 @@ const cars = require('./cars.json');
 
 
 module.exports = async function (context, req) {
+    const express = require('express');
+    const app = express();
+
+    //console.log('Listening on 3000');
+    app.use(express.json());
+
+    const cars = require('./cars.json');
+    const seeds = cars;
+
     context.log('JavaScript HTTP trigger function processed a request.');
     context.res = {
-        body: cars
+        body: seeds
     };
 }
 
--- api/message/index.js
+++ api/message/index.js
@@ -1,5 +1,5 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    context.log('JavaScript HTTP trigger function rlewfwfr a request.');
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
