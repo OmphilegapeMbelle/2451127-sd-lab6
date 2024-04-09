66cc65f2c058de290c74345d1275f9110acdf2d1
Omphilegape Mbelle <2451127@students.wits.ac.za>
2024-04-09 14:10:13 +0200

    ' '

--- api/cars/index.js
+++ api/cars/index.js
@@ -3,19 +3,19 @@ module.exports = async function (context, req) {
     const cars = require('./cars.json');
     const name = (req.query.name || (req.body && req.body.name));;
 
-    if (req.url === 'https://2992a9f0-a1b2-4df8-b9e6-19f659867cd2.azurewebsites.net/api/cars' && req.method === 'GET'){
+    /*if (req.url === 'https://2992a9f0-a1b2-4df8-b9e6-19f659867cd2.azurewebsites.net/api/cars' && req.method === 'GET'){
         const responseMessage = cars;
-    }
+    }*/
 
-    if (req.url === 'https://2992a9f0-a1b2-4df8-b9e6-19f659867cd2.azurewebsites.net/api/cars/:id' && req.method === 'GET'){
+    /*if (req.url === 'https://2992a9f0-a1b2-4df8-b9e6-19f659867cd2.azurewebsites.net/api/cars/:id' && req.method === 'GET'){
         const responseMessage = car;
-    }
+    }*/
 
     /*if (req.url === 'https://2992a9f0-a1b2-4df8-b9e6-19f659867cd2.azurewebsites.net/api/cars'){
         const responseMessage = cars;
     }*/
 
-    //const responseMessage = cars;
+    const responseMessage = req;
 
     context.res = {
         // status: 200, /* Defaults to 200 */
