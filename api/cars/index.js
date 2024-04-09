c503225541a2a83750a1b18cbafd40642bbf54ff
Omphilegape Mbelle <2451127@students.wits.ac.za>
2024-04-09 19:02:36 +0200

    ' '

--- api/cars/index.js
+++ api/cars/index.js
@@ -4,13 +4,17 @@ module.exports = async function (context, req) {
     const name = (req.query.name || (req.body && req.body.name));;
     let responseMessage = cars;
 
-    if (req.url === 'https://2992a9f0-a1b2-4df8-b9e6-19f659867cd2.azurewebsites.net/api/cars/:id' && (req.method === 'GET' || req.method === 'PUT')){
+    if (req.url === 'https://2992a9f0-a1b2-4df8-b9e6-19f659867cd2.azurewebsites.net/api/cars/:id' && req.method === 'GET'){
         responseMessage = cars;
     }
 
-    /*if (req.url === 'https://2992a9f0-a1b2-4df8-b9e6-19f659867cd2.azurewebsites.net/api/cars'){
-        const responseMessage = cars;
-    }*/
+    if (req.url === 'https://2992a9f0-a1b2-4df8-b9e6-19f659867cd2.azurewebsites.net/api/cars' && req.method === 'PUT'){
+        const id = req.params.id;
+        const updatedCar = req.body;
+        const index = cars.findIndex(car => car.id === id);
+        cars[req.params.id] = updatedCar;
+        responseMessage = updatedCar;
+    }
 
     context.res = {
         // status: 200, /* Defaults to 200 */
