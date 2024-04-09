2a219f34bca92a24290d74dacee28bd7843c5915
Omphilegape Mbelle <2451127@students.wits.ac.za>
2024-04-09 13:16:01 +0200

    ' '

--- api/cars/index.js
+++ api/cars/index.js
@@ -8,7 +8,5 @@ module.exports = async function (context, req) {
         body: responseMessage
     };
 
-    app.get('https://8ff60c99-b92a-43d1-a3db-ce56ca93a489.azurewebsites.net/api/cars', (req, res) => {
-        res.send(cars);
-    });
+    
 }
