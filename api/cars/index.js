207b2c5e6065697d2a655b52732ac06fa28770d5
Omphilegape Mbelle <2451127@students.wits.ac.za>
2024-04-09 10:33:37 +0200

    'so many'

--- api/cars/index.js
+++ api/cars/index.js
@@ -1,3 +1,15 @@
+module.exports = async function (context, req){
+    context.log('JavaScript HTTP trigger function processed a request.');
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+};
 
     const express = require('express');
     const app = express();
@@ -8,7 +20,7 @@
 
     
     //get all cars
-    module.exports = app.get('/api/cars', (req, res) => {
+    app.get('/api/cars', (req, res) => {
         res.send(cars);
     });
 
