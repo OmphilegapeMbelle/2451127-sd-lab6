cdd340135dea0cd6ea4ae595056463881b21e505
Omphilegape Mbelle <2451127@students.wits.ac.za>
2024-04-09 18:51:57 +0200

    ' '

--- api/cars/index.js
+++ api/cars/index.js
@@ -4,7 +4,7 @@ module.exports = async function (context, req) {
     const name = (req.query.name || (req.body && req.body.name));;
     let responseMessage = cars;
 
-    if (req.url === 'https://2992a9f0-a1b2-4df8-b9e6-19f659867cd2.azurewebsites.net/api/cars' && (req.method === 'GET' || req.method === 'PUT')){
+    if (req.url === 'https://2992a9f0-a1b2-4df8-b9e6-19f659867cd2.azurewebsites.net/api/cars/:id' && (req.method === 'GET' || req.method === 'PUT')){
         responseMessage = cars;
     }
 
