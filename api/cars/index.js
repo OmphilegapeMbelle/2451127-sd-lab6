86659cc28c29d09cd9cad9b124a2ac701b24412f
Omphilegape Mbelle <2451127@students.wits.ac.za>
2024-04-09 19:33:08 +0200

    ' '

--- api/cars/index.js
+++ api/cars/index.js
@@ -4,21 +4,23 @@ module.exports = async function (context, req) {
     const name = (req.query.name || (req.body && req.body.name));;
     let responseMessage = cars;
 
-    /*if (req.url === 'https://2992a9f0-a1b2-4df8-b9e6-19f659867cd2.azurewebsites.net/api/cars/:id' && req.method === 'GET'){
+    if (req.url === 'https://2992a9f0-a1b2-4df8-b9e6-19f659867cd2.azurewebsites.net/api/cars/:id' && req.method === 'GET'){
         responseMessage = cars;
-    }*/
+    }
 
     if (req.url === 'https://2992a9f0-a1b2-4df8-b9e6-19f659867cd2.azurewebsites.net/api/cars' && req.method === 'POST'){
         const newCar = req.body;
         newCar.id = cars.length + 1;
         cars.push(newCar);
         responseMessage = cars;
+
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: responseMessage
+        };
     }
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    
 
     
 }
