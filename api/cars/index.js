9ffc9a72e380b44c010ccbeb62425ad3b456a07e
Omphilegape Mbelle <2451127@students.wits.ac.za>
2024-04-09 13:27:11 +0200

    ' '

--- api/cars/index.js
+++ api/cars/index.js
@@ -1,12 +1,26 @@
 //create cars api using express
 module.exports = async function (context, req) {
-    const name = (req.query.name || (req.body && req.body.name));;
-    const responseMessage = req;
+    try {
+        const { method, url, originalUrl, headers } = req;
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+        // Log the request details for debugging
+        context.log('Request Method:', method);
+        context.log('Request URL:', url);
+        context.log('Original URL:', originalUrl);
+        context.log('Request Headers:', headers);
 
+        // Make a GET request to the specified URL
+        const response = await axios.get(url);
+
+        context.res = {
+            status: response.status,
+            body: response.data
+        };
+    } catch (error) {
+        context.res = {
+            status: error.response.status || 500,
+            body: error.response.data || 'Internal Server Error'
+        };
+    }
     
 }
