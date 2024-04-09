d37f1f77c7e3b7026c37f4a9a7c1c11e14b3283b
Omphilegape Mbelle <2451127@students.wits.ac.za>
2024-04-09 11:19:39 +0200

    ' '

--- api/cars/index.js
+++ api/cars/index.js
@@ -7,9 +7,13 @@ module.exports = async function (context, req) {
 
     const cars = require('./cars.json');
     const seeds = cars;
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function hgdtfgtee successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-    context.log('JavaScript HTTP trigger function processed a request.');
+    context.log(cars);
     context.res = {
-        body: seeds
+        body: responseMessage
     };
 }
