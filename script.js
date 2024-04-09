e3b80ece78d88e49d511796d33518ecfd9646f5a
Omphilegape Mbelle <2451127@students.wits.ac.za>
2024-04-09 14:49:12 +0200

    ' '

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const carList = document.getElementById('carList');
     cars = [];
     loadCarsBtn.addEventListener('click', () => {
-        fetch('https://mango-desert-0addf3403.5.azurestaticapps.net/api/cars')
+        fetch('./api/cars')
             .then(response => response.json())
             .then(data => {
                 cars = data;
