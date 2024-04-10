df0f4185040eef29bb1e03d9b432eca8495a4ae3
Omphilegape Mbelle <2451127@students.wits.ac.za>
2024-04-10 11:40:17 +0200

    ' '

--- script.js
+++ script.js
@@ -79,6 +79,7 @@ function removeCar(index) {
 carList.addEventListener('click', event => {
     if (event.target.classList.contains('btn-remove')) {
         const index = event.target.dataset.index;
+        console.log(index);
         removeCar(index);
     }
 });
